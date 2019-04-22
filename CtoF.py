1a32e598b5cc815c335a3e28b114ea9ddcddaee0
Lester Lai <lester.lai.tw@gmail.com>
2019-04-23 05:41:16 +0800

    Add new file

---
+++ CtoF.py
@@ -0,0 +1,12 @@
+#整數 integer int
+#浮點數(小數) float
+#布林值 boolean bool (True, False)
+#字串 string str (文字)
+
+#rain = input('請問今天有沒有下雨:')
+#if rain == '有':
+#	print('請在家看電影!')
+
+f = input('攝氏溫度→華氏溫度，請輸入攝氏溫度: ')
+f = float( f ) * ( 9 / 5 ) + 32
+print('華氏溫度:',f )
