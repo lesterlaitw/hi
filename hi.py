6aae43e67f94ac96b5219be3833628be891ef966
Lester Lai <lester.lai.tw@gmail.com>
2019-04-23 05:16:39 +0800

    CtoF

--- hi.py
+++ hi.py
@@ -2,5 +2,11 @@
 #浮點數(小數) float
 #布林值 boolean bool (True, False)
 #字串 string str (文字)
-name = input('請輸入名字:')
-print('嗨!',name)
+
+#rain = input('請問今天有沒有下雨:')
+#if rain == '有':
+#	print('請在家看電影!')
+
+f = input('攝氏溫度→華氏溫度，請輸入攝氏溫度:')
+f = int( f ) * (9 / 5 ) + 32
+print('華氏溫度:',f )
